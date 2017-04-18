59c5426e18e2de020c431543a23dca9381e99a77
Tyler Garcia <t-garcia15@hotmail.com>
2017-04-18 17:09:42 -0400

    added dummy data

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,3 +1,25 @@
 function AppCtrl() {
-	console.log("Hello World from controller")
+	console.log("Hello World from controller");
+
+	person1 = {
+		name: 'Tyler',
+		email: 'tyler@email.com',
+		number: '(111) 111-1111'
+	};
+
+	person2 = {
+		name: 'Marcial',
+		email: 'marcial@email.com',
+		number: '(222) 222-2222'
+	};
+
+	person3 = {
+		name: 'Luciano',
+		email: 'luchi@email.com',
+		number: '(333) 333-3333'
+	};
+
+	var contactlist = [person1, person2, person3];
+	$scope.contactlist = contactlist;
+	
 }
